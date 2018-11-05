79e78d49bcb4c50d057ca828d702d631ff6f4ab0
Wang Lisa <wanglisa@Wangs-MacBook-Air.local>
2018-11-05 17:13:26 +0800

    upgrade to angular 7 and add the anuglar material and cdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CityComponent } from './city/city.component';
+import { CountryComponent } from './country/country.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatTabsModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityComponent,
+    CountryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScrollingModule,
+    MatTabsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
